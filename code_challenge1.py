99283b044680a966c8086d09987dbceb2638e496
diaaa000 <salumbidesprincessdiane@gmail.com>
2025-08-15 14:50:10 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name  = input()
+print("\t\t\t\t* \n\n\t\t\t*\n\n\t\t*\t\t\t*\n\t*\n\n\t\t\t\t Hello Peeps\t\t*\n\n*\t\t\t\t", "\t\t\t\t*\n\n\t*\t\t\t\t\t\t*\n\n\t\t*\n\n\t\t\t\t\t\t*\n\n \t\t\t\t\t*\n\t\t\t*\n\n\t\t\t\t*")
