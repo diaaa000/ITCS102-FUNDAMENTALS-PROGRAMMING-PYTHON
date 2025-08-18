f3e17cb31b392b8bd81af7e117481163c90632bd
yanu <salumbidesprincessdiane@gmail.com>
2025-08-18 11:25:55 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,10 @@
+a = 13
+
+print("The value of a is ", a)
+
+a +=3
+a -=4
+a *=5
+a /=6 
+
+print("The vaue of a is ", a)
---
+++ activity9.py
@@ -0,0 +1,6 @@
+username = 'yannie'
+password = 'broccoli' 
+
+print(username == 'yannie')
+print((username == 'yannie') and (password != 'broccoli'))
+print((username == 'yannie') or (password == 'broccoli'))
