2a30df25d02ddbe28445d1cb6f77e7ae514981ae
yanu <salumbidesprincessdiane@gmail.com>
2025-08-27 15:01:07 +0800

    Create code_challenge3.py

---
+++ code_challenge3.py
@@ -0,0 +1,13 @@
+#code_challenge3
+#username_and_password
+
+username = input('input username')
+p_input = input('enter password')
+
+if username == uname and pword == p_input:
+  print("Access granted")
+
+else:
+  print("Access denied")
+  
+
