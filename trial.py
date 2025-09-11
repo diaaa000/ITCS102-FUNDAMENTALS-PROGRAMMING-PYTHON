2be9736a9e0ad6d0d726be67f795557a308d6d3e
yanu <salumbidesprincessdiane@gmail.com>
2025-09-11 22:55:22 +0800

    Update trial.py

--- trial.py
+++ trial.py
@@ -9,15 +9,15 @@ if genre == "Adventure":
     if length == "Short":
         decade = input("\nWhich decade do you want your Adventure Manga? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nHere are the Long Adventure Mangas in the year 2000s:\n\tNaruto by Masashi Kishimoto\n\tHunter X Hunter by Yoshihiro Togashi")
+            print("\nHere are the Short Adventure Manga's in the year 2000s:\n\tNaruto by Masashi Kishimoto\n\tHunter X Hunter by Yoshihiro Togashi")
         elif decade == "2010s":
-            print("\nHere are the Long Adventure Mangas in the year 2010s:\n\tOne piece by Eiichiro Oda\n\tDragon ball by Akira Toriyama")
+            print("\nHere are the Short Adventure Manga's in the year 2010s:\n\tOne piece by Eiichiro Oda\n\tDragon ball by Akira Toriyama")
     elif length == "Long":
-        decade = input("\nWhich decade? (2000s, 2010s): ")
+        decade = input("\nWhich decade do you want your Adventure Manga? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nHere are the Short Adventure Mangas in the year 2000s:\n\tInuyasha by Rumiko Takahashi\n\tThe Cat Returns by Reiko Yoshida")
+            print("\nHere are the Long Adventure Manga's in the year 2000s:\n\tInuyasha by Rumiko Takahashi\n\tThe Cat Returns by Reiko Yoshida")
         elif decade == "2010s":
-            print("\nHere are the Short Adventure Mangas in the year 2010s:\n\tNoragami by Adachitoka\n\tTower of God by Lee Jong-hui")
+            print("\nHere are the Long Adventure Manga's in the year 2010s:\n\tNoragami by Adachitoka\n\tTower of God by Lee Jong-hui")
 
 #Horror
 elif genre == "Horror":
@@ -25,37 +25,30 @@ elif genre == "Horror":
     if length == "Short":
         decade = input("\nWhich decade do you want your Horror Manga be? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nHere are the Long Horror Mangas in the year 2000s:\n\tDeath Note by Tsugumi Ohba\n\tUzumaki by Junji Ito")
+            print("\nHere are the Short Horror Manga's in the year 2000s:\n\tDeath Note by Tsugumi Ohba\n\tUzumaki by Junji Ito")
         elif decade == "2010s":
-            print("\nHere are the Long Horror Mangas in the year 2010s:\n\tShiver by Junji Ito\n\tTokyo Ghoul by Sui Ishida")
+            print("\nHere are the Short Horror Manga's in the year 2010s:\n\tShiver by Junji Ito\n\tTokyo Ghoul by Sui Ishida")
     elif length == "Long":
         decade = input("\nWhich decade do you want your Horror Manga be? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nHere are the Short Horror Mangas in the year 2000s:\n\tTomie by Junji Ito\n\tMonster by Naoki Urasawa")
+            print("\nHere are the Long Horror Manga's in the year 2000s:\n\tTomie by Junji Ito\n\tMonster by Naoki Urasawa")
         elif decade == "2010s":
-            print("\nHere are the Short Horror Mangas in the year 2010s:\n\tAnother by Yukito Ayatsuji\n\tPumpkin Night by Masaya Hokazono")
+            print("\nHere are the Long Horror Manga's in the year 2010s:\n\tAnother by Yukito Ayatsuji\n\tPumpkin Night by Masaya Hokazono")
    
 #Romance
 elif genre == "Romance":
-    length = input("\nHow long should the manga be? (Short, Long): ")
-    if duration == "short":
-        decade = input("\nWhich decade? (2000s, 2010s): ")
+    length = input("\nHow long do you want your manga be? (Short, Long): ")
+    if length == "Short":
+        decade = input("\nWhich decade do you want your Romance Manga be? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nI can recommend:\n\tThe Enigma of Amigara Fault by Junji Ito")
+            print("\nHere are the Short Romance Manga's in the year 2000s:\n\tFruits Basket by Natsuki Takaya\n\tKaichou wa Maid Sama by Hiro Fujiwara")
         elif decade == "2010s":
-            print("\nI can recommend:\n\tFragments of Horror by Junji Ito")
-    elif duration == "medium":
-        decade = input("\nWhich decade? (2000s, 2010s): ")
+            print("\nHere are the Short Romance Manga's in the year 2010s:\n\tHorimiya by Daisuke Hagiwara\n\tAo Haru Ride by Io Sakisaka")
+    elif length == "Long":
+        decade = input("\nWhich decade do you want your Romance Manga be? (2000s, 2010s): ")
         if decade == "2000s":
-            print("\nI can recommend:\n\tThe Drifting Classroom by Kazuo Umezu")
+            print("\nHere are the Long Romance Manga's in the year 2000s:\n\tKamisama Hajimemashita by Julietta Suzuki\n\tOuran High School Host Club by Bisco Hatori ")
         elif decade == "2010s":
-            print("\nI can recommend:\n\tI Am a Hero by Kengo Hanazawa")
-    elif duration == "long":
-        decade = input("\nWhich decade? (2000s, 2010s): ")
-        if decade == "2000s":
-            print("\nI can recommend:\n\tHellsing by Kouta Hirano")
-        elif decade == "2010s":
-            print("\nI can recommend:\n\tAjin: Demi-Human by Gamon Sakurai")
-
+            print("\nHere are the Long Romance Manga's in the year 2010s:\n\tHoney Lemon Soda by Murata Mayu"\n\tSnow White with The Red Hair by Sorata Akituki")
 else:
-    print("\n\tSorry but that genre isn't available. Please try again.")
+    print("\n\tSorry but the genre you want is not available right now, please Choose another one")
