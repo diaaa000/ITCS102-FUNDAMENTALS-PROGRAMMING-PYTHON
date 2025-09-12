fc8dea1da46085bece9a507dae99414c6d375a4b
yanu <salumbidesprincessdiane@gmail.com>
2025-09-12 19:00:35 +0800

    Create activity13.py

---
+++ activity13.py
@@ -0,0 +1,9 @@
+#using loop, ask user to input 15 numbers
+#after input, get the summation of all numbers 
+
+numero = 5
+for y in range(1,15):
+  n = eval(input("Enter a number")) 
+  numero += n 
+  print('total number ay', numero) 
+  print(numero) 
