61063c9c3bb112cd1e0a3678b8603734955f7ef0
yanu <salumbidesprincessdiane@gmail.com>
2025-09-14 10:14:04 +0800

    Update activity5.py

--- activity5.py
+++ activity5.py
@@ -1,5 +1,6 @@
-something = eval(input("Input something -->"))
+something = eval(input("Input something --> "))
 
 print("The data type of something is",type(something))
 answer = 24 + something
+
 print("the answer is", answer)
