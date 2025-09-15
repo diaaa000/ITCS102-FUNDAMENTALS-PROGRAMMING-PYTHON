cac429b488d309d1bb84da7f786a29535b539837
yanu <salumbidesprincessdiane@gmail.com>
2025-09-15 18:14:27 +0800

    Create code_challenge7.py

---
+++ code_challenge7.py
@@ -0,0 +1,11 @@
+#summation of odd numbers
+#loop 10 times
+
+sum_of_odd = 0
+for y in range(10):
+	n = int(input(f"Enter an odd number: "))
+	if n % 2 != 0:
+		sum_of_odd += n
+		
+		
+print("The sum of all the odd numbers is", sum_of_odd)
