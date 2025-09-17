2c4f2b269087718c6d76a4c7fdd4a6770ea25bcf
yanu <salumbidesprincessdiane@gmail.com>
2025-09-17 11:01:05 +0800

    Create code_challenge8.py

---
+++ code_challenge8.py
@@ -0,0 +1,9 @@
+#MULTIPLICATION TABLE
+
+an = int(input("Enter a number: --> "))
+
+print(f"Multiplication Table for {an}: ")
+
+for y in range(1,11):
+	result = an * y
+	print(f"{an} * {y} = {result}")
