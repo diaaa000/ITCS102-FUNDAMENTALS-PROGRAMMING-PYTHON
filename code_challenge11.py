4f48cf3282747b4c4f065dfbd5c8fd5fa6829cb5
yanu <salumbidesprincessdiane@gmail.com>
2025-09-22 21:00:43 +0800

    Create code_challenge11.py

---
+++ code_challenge11.py
@@ -0,0 +1,18 @@
+
+
+for i in range(1, 11, 1):
+	for j in range(10, i, -1):
+		print(" ", end= '  ')
+	for x in range(1, i, 1):
+		print(" * ", end= " ")
+	for z in range(1, i, 1):
+		print(" * ", end= " ")
+	print()
+for i in range(1, 11, 1):
+	for j in range(1, i, 1):
+		print(" ", end= "  ")
+	for x in range(10, i, -1):
+		print(" * ", end= " ")
+	for z in range(10, i, -1):
+		print(" * ", end= " ")
+	print()
