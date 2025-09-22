48f14ff6a693c7320d2346f5023d27445e132f53
yanu <salumbidesprincessdiane@gmail.com>
2025-09-22 20:59:32 +0800

    Create code_challenge10.py

---
+++ code_challenge10.py
@@ -0,0 +1,20 @@
+
+
+for i in range(1, 11, 1):
+	for j in range(10, i, -1):
+		print(" ", end= '  ')
+	for x in range(1, i, 1):
+		print(" * ", end= " ")
+	for z in range(1, i, 1):
+		print(" * ", end= " ")
+	print()
+print("\t\t *", end=" ")
+for i in range(1, 11, 1):
+	for j in range(10, i, -1):
+		print(" ", end= '  ')
+	for x in range(1, i, 1):
+		print(" * ", end= " ")
+	for z in range(1, i, 1):
+		print(" * ", end= " ")
+	print()
+
