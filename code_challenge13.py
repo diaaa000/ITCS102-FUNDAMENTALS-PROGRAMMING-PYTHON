ce586ecff2d8962241c14e4d86a4da5097c3808f
yanu <salumbidesprincessdiane@gmail.com>
2025-10-01 18:51:31 +0800

    Create code_challenge13.py

---
+++ code_challenge13.py
@@ -0,0 +1,52 @@
+#xmasturiii
+for a in range(2, 6, 1):
+	for b in range(0, 7):
+		print(" ", end=" ")
+	for c in range(6, a, -1):
+		print(" ", end=" ")
+	for d in range(3, a):
+		print("¬", end=" ")
+	for e in range(2, a, 1):
+		print("¬", end=" ")
+	print()
+for a2 in range(1, 3, 1):
+	for b2 in range(0, 7):
+		print(" ", end=" ")
+	for c2 in range(0 - 1, a2, 1):
+		print(" ", end=" ")
+	for d2 in range(2, a2, -1):
+		print("¬", end=" ")
+	for e2 in range(3, a2, -1):
+		print("¬", end=" ")
+	print()
+for a3 in range(2, 9, 1):
+	for b3 in range(12, a3, -1):
+		print(" ", end=" ")
+	for c3 in range(2, a3, 1):
+		print("¬", end=" ")
+	for d3 in range(1, a3):
+		print("¬", end=" ")
+	print()
+for a4 in range(2, 10, 1):
+	for b4 in range(12, a4, -1):
+		print(" ", end=" ")
+	for c4 in range(1, a4):
+		print("¬", end=" ")
+	for d4 in range(2, a4):
+		print("¬", end=" ")
+	print()
+for a5 in range(2, 13, 1):
+	for b5 in range(12, a5, -1):
+		print(" ", end=" ")
+	for c5 in range(1, a5):
+		print("¬", end=" ")
+	for d5 in range(2, a5):
+		print("¬", end=" ")
+	print()
+for a6 in range(1,7):
+	for b6 in range(1,9):
+		print(" ", end=" ")
+	for c6 in range(1,9):
+		print("", end="¬")
+	print()
+			
