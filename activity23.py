3823282f12a60cf1735b9656633f4d0c1eb25431
yanu <salumbidesprincessdiane@gmail.com>
2025-11-02 19:42:49 +0800

    Create activity23.py

---
+++ activity23.py
@@ -0,0 +1,32 @@
+
+# months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"]
+
+# print(months)
+# months.append("Aug")
+# months.append("Sept")
+# months.append("Oct")
+# months.append("Nov")
+# months.append("Dec")
+# print(months)
+# months.pop()
+# print(months)
+# months.append("Dec")
+# print(months)
+
+# #iteration
+# for month in months:
+#       print(f"{month}, 2025")
+
+# for m in months[8 : 12]:
+#	   print(f"The bermonths are: {m}")
+
+# name
+full_name = "Princess Diane B. Salumbides"
+name = list(full_name)
+print(name)
+
+for i in full_name[0 : 4]:
+	print(i)
+	
+name.reverse()
+print(name)
