2d5b3a58efd70b76637991235fb1080f2c11c97e
yanu <salumbidesprincessdiane@gmail.com>
2025-11-16 19:28:47 +0800

    Create activity26.py

---
+++ activity26.py
@@ -0,0 +1,8 @@
+
+
+color_meaning = ['Pink', 'Blue', 'Green', 'Red', 'Brown', 'Yellow']
+
+#dictionary
+color_meaning_1 = {'girly' : 'Pink', 'peace' : 'Blue', 'nature' : 'Green', 'love' : 'Red','comfort' : 'Brown', 'joy' : 'Yellow'}
+
+print(color_meaning_1['girly'])
