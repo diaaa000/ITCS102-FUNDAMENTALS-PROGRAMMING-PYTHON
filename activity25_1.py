60ad227b06ccee3961310e55a5c7f9e644729461
yanu <salumbidesprincessdiane@gmail.com>
2025-11-16 19:26:30 +0800

    Create activity25_1.py

---
+++ activity25_1.py
@@ -0,0 +1,18 @@
+
+def activity1():
+	x = 5
+	print("The value of x is ", x)
+	
+def activity2():
+	name = input("What is your name?")
+	print("Hi", name , "Welcome to the game")
+	
+def add_odd():
+	odd_sum = 0
+	for i in range(1, 11, 1):
+		num = eval(input(f"{i} - Enter a number --> "))
+		
+		if num % 2 == 1:
+			odd_sum += num
+			
+		print(f"The sum of all the ODD numbers given is {odd_sum}")
