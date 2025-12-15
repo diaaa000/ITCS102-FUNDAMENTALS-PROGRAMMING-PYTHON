6174af98fd1893602200681ae18d3a487feaeb2c
yanu <salumbidesprincessdiane@gmail.com>
2025-12-15 08:58:18 +0800

    Update and rename Finals_Project to Finals_Project.py

--- Finals_Project.py
+++ Finals_Project.py
@@ -144,7 +144,7 @@ def activity17():
     for y in range(1, 11, 1):
      for i in range(1, y, 1):
 	       print(i, end="  ")
-print()
+    print()
 
 def activity18():
     for x in range(1, 11, 1):
